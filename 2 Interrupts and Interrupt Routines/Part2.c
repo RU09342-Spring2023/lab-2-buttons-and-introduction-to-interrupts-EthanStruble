d8df864777af0276d6703d15450d3215c830b1ae
EthanStruble <113445493+EthanStruble@users.noreply.github.com>
2023-02-27 09:46:48 -0500

    Fixed
    Tried to upload it from Gitkraken about a week ago and it didn't work. So im doing this.

--- 2 Interrupts and Interrupt Routines/Part2.c
+++ 2 Interrupts and Interrupt Routines/Part2.c
@@ -27,7 +27,9 @@ int main(void)
     P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
     P1DIR |= BIT0;                          // Set P1.0 to output direction
 
-    // @TODO You need to add in the configuration for the Green LED
+    P6OUT &= ~BIT6;                          // Clear P6.6 output latch for a defined power-on state
+    P6DIR |= BIT6;                          // Set P6.6 to output direction
+
 
     P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
     P2REN |= BIT3;                          // P2.3 pull-up register enable
@@ -46,9 +48,15 @@ int main(void)
     {
         // @TODO You will need to modify this code to change between blinking the Red LED or the Green LED
         if (ToggleEnable)
+        {
             P1OUT ^= BIT0;                  // P1.0 = toggle
+            P6OUT &= ~BIT6;                 //This turns off Port 6.6 so the led doesn't turn on
+        }
         else
+        {
+            P6OUT ^= BIT6;                   //Toggling Green LED
             P1OUT &= ~BIT0;                 // Set P1.0 to 0
+        }
         __delay_cycles(100000);
     }
 }
